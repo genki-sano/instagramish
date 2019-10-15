9bfe1170f08a9c764247d33e4358b2f42d86685a
g-sano <g.sano.2441@gmail.com>
2019-10-16 01:41:40 +0900

    Prepare XMLHttpRequest

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,18 @@
+// CSRF対策
+import './bootstrap'
+
 import Vue from 'vue'
 // ルーティングの定義をインポート
 import router from './router'
+// ストアの定義をインポート
+import store from './store'
 // ルートコンポーネントをインポート
 import App from './App.vue'
 
 new Vue({
     el: '#app',
     router, // ルーティングの定義を読み込む
-    components: { App }, // ルートコンポーネントの使用を宣言
+    store, // ストアの定義を読み込む
+    components: {App}, // ルートコンポーネントの使用を宣言
     template: '<App />' // ルートコンポーネントを描画
 });
