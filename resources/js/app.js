cfa03635b95ddc7c218805e3d28717fb603d6b8a
g-sano <g.sano.2441@gmail.com>
2019-10-19 07:45:29 +0900

    Stay logged in when updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,10 +9,17 @@ import store from './store'
 // ルートコンポーネントをインポート
 import App from './App.vue'
 
-new Vue({
-    el: '#app',
-    router, // ルーティングの定義を読み込む
-    store, // ストアの定義を読み込む
-    components: {App}, // ルートコンポーネントの使用を宣言
-    template: '<App />' // ルートコンポーネントを描画
-});
+const createApp = async () => {
+    // ログインチェック
+    await store.dispatch('auth/currentUser');
+
+    new Vue({
+        el: '#app',
+        router, // ルーティングの定義を読み込む
+        store, // ストアの定義を読み込む
+        components: {App}, // ルートコンポーネントの使用を宣言
+        template: '<App />' // ルートコンポーネントを描画
+    });
+};
+
+createApp();
