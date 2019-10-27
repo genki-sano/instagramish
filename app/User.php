57b168cf8cf1c68f033b37cd219d198b5a1c08df
g-sano <g.sano.2441@gmail.com>
2019-10-27 23:15:28 +0900

    Update commnet out

--- app/User.php
+++ app/User.php
@@ -11,20 +11,12 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    /** @var array 複数代入する属性 */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be visible for arrays.
-     *
-     * @var array
-     */
+    /** @var array JSONに含める属性 */
     protected $visible = [
         'name'
     ];
