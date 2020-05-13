42d67484e8e38c5173877739ccb5fc52dc7e5dc2
g-sano <g.sano.2441@gmail.com>
2020-05-13 20:54:23 +0900

    Code format

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -6,7 +6,6 @@ use App\Comment;
 use App\Http\Requests\StoreComment;
 use App\Http\Requests\StorePhoto;
 use App\Photo;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -28,7 +27,8 @@ class PhotoController extends Controller
     public function index()
     {
         $photos = Photo::with(['owner', 'likes'])
-            ->orderBy(Photo::CREATED_AT, 'desc')->paginate();
+            ->orderBy(Photo::CREATED_AT, 'desc')
+            ->paginate();
 
         return $photos;
     }
@@ -75,7 +75,9 @@ class PhotoController extends Controller
      */
     public function show(string $id)
     {
-        $photo = Photo::where('id', $id)->with(['owner', 'comments.author', 'likes'])->first();
+        $photo = Photo::where('id', $id)
+            ->with(['owner', 'comments.author', 'likes'])
+            ->first();
 
         return $photo ?? abort(404);
     }
@@ -137,13 +139,16 @@ class PhotoController extends Controller
      */
     public function addComment(Photo $photo, StoreComment $request)
     {
-        $comment = new Comment();
-        $comment->content = $request->get('content');
-        $comment->user_id = Auth::user()->id;
+        $comment = new Comment([
+            'content' => $request->get('content'),
+            'user_id' => Auth::user()->id
+        ]);
         $photo->comments()->save($comment);
 
         // authorリレーションをロードするためにコメントを取得しなおす
-        $new_comment = Comment::where('id', $comment->id)->with('author')->first();
+        $new_comment = Comment::where('id', $comment->id)
+            ->with('author')
+            ->first();
 
         return response($new_comment, 201);
     }
@@ -155,7 +160,9 @@ class PhotoController extends Controller
      */
     public function like(string $id)
     {
-        $photo = Photo::where('id', $id)->with('likes')->first();
+        $photo = Photo::where('id', $id)
+            ->with('likes')
+            ->first();
 
         if (! $photo) {
             abort(404);
@@ -174,7 +181,9 @@ class PhotoController extends Controller
      */
     public function unlike(string $id)
     {
-        $photo = Photo::where('id', $id)->with('likes')->first();
+        $photo = Photo::where('id', $id)
+            ->with('likes')
+            ->first();
 
         if (! $photo) {
             abort(404);
